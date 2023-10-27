23f58a2ca033ea5f63c4fa343f3aa77f6335222b
Vitto-Mazeto <vittopicma@gmail.com>
2023-10-27 16:16:55 -0300

    add: semana 3 simon game

---
+++ SEMANA_03/01_CODIGOS/simon_esp.ino
@@ -0,0 +1,104 @@
+const uint8_t BOTOES[] = {16, 17, 18, 19};
+const uint8_t LEDS[] = {27, 26, 25, 33};
+const uint8_t BOTAO_RESET = 32;
+#define TAMANHO_JOGO 4
+
+uint8_t sequenciaJogo[TAMANHO_JOGO] = {0};
+uint8_t indexJogo = 0;
+
+void setup() {
+  Serial.begin(9600);
+  initializePins();
+  randomSeed(analogRead(4));
+}
+
+void initializePins() {
+  for (byte i = 0; i < 4; i++) {
+    pinMode(LEDS[i], OUTPUT);
+    pinMode(BOTOES[i], INPUT_PULLUP);
+  }
+  pinMode(BOTAO_RESET, INPUT_PULLUP);
+}
+
+void lightLED(byte ledIndex) {
+  digitalWrite(LEDS[ledIndex], HIGH);
+  delay(300);
+  digitalWrite(LEDS[ledIndex], LOW);
+}
+
+void iniciarSequencia() {
+  for (int i = 0; i < indexJogo; i++) {
+    byte ledAtual = sequenciaJogo[i];
+    lightLED(ledAtual);
+    delay(50);
+  }
+}
+
+byte apertarBotao() {
+  while (true) {
+    if (digitalRead(BOTAO_RESET) == LOW) {
+      resetarJogo();
+      return 0;
+    }
+    for (byte i = 0; i < 4; i++) {
+      if (digitalRead(BOTOES[i]) == LOW) {
+        return i;
+      }
+    }
+    delay(1);
+  }
+}
+
+void resetarJogo() {
+  lightLED(1);
+  delay(80);
+  lightLED(1);
+  delay(80);
+  lightLED(1);
+  delay(80);
+  lightLED(1);
+  Serial.print("Perdeu");
+  delay(80);
+  indexJogo = 0;
+  delay(200);
+}
+
+bool checarSequencia() {
+  for (int i = 0; i < indexJogo; i++) {
+    byte expectedButton = sequenciaJogo[i];
+    byte actualButton = apertarBotao();
+    lightLED(actualButton);
+    if (expectedButton != actualButton) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void loop() {
+  if (indexJogo == TAMANHO_JOGO) {
+    while (digitalRead(BOTAO_RESET) != LOW) {
+      lightLED(2);
+      delay(80);
+    }
+    resetarJogo();
+    return;
+  }
+
+  sequenciaJogo[indexJogo] = random(0, 4);
+  indexJogo++;
+  Serial.print("Game index: ");
+  Serial.println(indexJogo);
+
+  iniciarSequencia();
+
+  if (!checarSequencia()) {
+    resetarJogo();
+  }
+
+  delay(300);
+
+  if (indexJogo > 0) {
+    delay(300);
+  }
+}
