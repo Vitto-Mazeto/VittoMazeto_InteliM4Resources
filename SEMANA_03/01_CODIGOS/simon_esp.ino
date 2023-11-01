3ac524af9c22e597be5aebecfba42f0fb80b6957
Vitto-Mazeto <vittopicma@gmail.com>
2023-11-01 09:37:16 -0300

    add: new esp

--- SEMANA_03/01_CODIGOS/simon_esp.ino
+++ SEMANA_03/01_CODIGOS/simon_esp.ino
@@ -1,47 +1,62 @@
 const uint8_t BOTOES[] = {16, 17, 18, 19};
 const uint8_t LEDS[] = {27, 26, 25, 33};
 const uint8_t BOTAO_RESET = 32;
+const uint8_t BUZZER_PIN = 22;
 #define TAMANHO_JOGO 4
 
 uint8_t sequenciaJogo[TAMANHO_JOGO] = {0};
 uint8_t indexJogo = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   initializePins();
   randomSeed(analogRead(4));
 }
 
-void initializePins() {
-  for (byte i = 0; i < 4; i++) {
+void initializePins()
+{
+  for (byte i = 0; i < 4; i++)
+  {
     pinMode(LEDS[i], OUTPUT);
     pinMode(BOTOES[i], INPUT_PULLUP);
   }
   pinMode(BOTAO_RESET, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
-void lightLED(byte ledIndex) {
+void lightLED(byte ledIndex)
+{
   digitalWrite(LEDS[ledIndex], HIGH);
   delay(300);
   digitalWrite(LEDS[ledIndex], LOW);
 }
 
-void iniciarSequencia() {
-  for (int i = 0; i < indexJogo; i++) {
+void iniciarSequencia()
+{
+  for (int i = 0; i < indexJogo; i++)
+  {
     byte ledAtual = sequenciaJogo[i];
     lightLED(ledAtual);
+    tone(BUZZER_PIN, 500);
     delay(50);
+    noTone(BUZZER_PIN);
   }
 }
 
-byte apertarBotao() {
-  while (true) {
-    if (digitalRead(BOTAO_RESET) == LOW) {
+byte apertarBotao()
+{
+  while (true)
+  {
+    if (digitalRead(BOTAO_RESET) == LOW)
+    {
       resetarJogo();
       return 0;
     }
-    for (byte i = 0; i < 4; i++) {
-      if (digitalRead(BOTOES[i]) == LOW) {
+    for (byte i = 0; i < 4; i++)
+    {
+      if (digitalRead(BOTOES[i]) == LOW)
+      {
         return i;
       }
     }
@@ -49,13 +64,18 @@ byte apertarBotao() {
   }
 }
 
-void resetarJogo() {
+void resetarJogo()
+{
+  lightLED(1);
+  tone(BUZZER_PIN, 500);
+  delay(80);
+  tone(BUZZER_PIN, 250);
   lightLED(1);
   delay(80);
+  tone(BUZZER_PIN, 300);
   lightLED(1);
   delay(80);
-  lightLED(1);
-  delay(80);
+  tone(BUZZER_PIN, 750);
   lightLED(1);
   Serial.print("Perdeu");
   delay(80);
@@ -63,21 +83,27 @@ void resetarJogo() {
   delay(200);
 }
 
-bool checarSequencia() {
-  for (int i = 0; i < indexJogo; i++) {
+bool checarSequencia()
+{
+  for (int i = 0; i < indexJogo; i++)
+  {
     byte expectedButton = sequenciaJogo[i];
     byte actualButton = apertarBotao();
     lightLED(actualButton);
-    if (expectedButton != actualButton) {
+    if (expectedButton != actualButton)
+    {
       return false;
     }
   }
   return true;
 }
 
-void loop() {
-  if (indexJogo == TAMANHO_JOGO) {
-    while (digitalRead(BOTAO_RESET) != LOW) {
+void loop()
+{
+  if (indexJogo == TAMANHO_JOGO)
+  {
+    while (digitalRead(BOTAO_RESET) != LOW)
+    {
       lightLED(2);
       delay(80);
     }
@@ -92,13 +118,15 @@ void loop() {
 
   iniciarSequencia();
 
-  if (!checarSequencia()) {
+  if (!checarSequencia())
+  {
     resetarJogo();
   }
 
   delay(300);
 
-  if (indexJogo > 0) {
+  if (indexJogo > 0)
+  {
     delay(300);
   }
 }
